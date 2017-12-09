d608b8cbc47f92a63a50ed5be715ccf0cb03796a
David Taylor <davetaylor@apple.com>
2017-12-08 19:45:38 -0800

    looks like there is a bug

---
+++ src/advent_of_code_2018/day1.clj
@@ -0,0 +1,27 @@
+(ns advent-of-code-2018.day1
+  (:require [clojure.string :refer [split]]
+            [clojure.tools.trace :refer [trace]]))
+
+(defn tupleize [l] (->> (butlast l) 
+                        (cons (last l)) 
+                        (map (fn [x y] [x y]) l)))
+
+(defn captcha [s]
+  (->> s
+       (partition 2 1)
+       (cons (list (first s) (last s)))
+       (filter #(= (first %) (second %)))
+       (map first)
+       (map #(Character/digit % 10))
+       (reduce +)))
+
+(defn to-digit [c] (- (int c) 48))
+
+(defn captcha-amir
+    [input]
+      (->> input
+           (cons (last input))
+           (partition 2 1)
+           (filter #(apply = %))
+           (map (comp to-digit first))
+           (apply +)))
---
+++ test/advent_of_code_2018/day1_test.clj
@@ -0,0 +1,32 @@
+(ns advent-of-code-2018.day1-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code-2018.day1 :refer :all]))
+(def intstream (-> "input/day1.txt" slurp))
+
+(deftest first-test
+    (testing "three"
+      (is (= 3 (captcha "1122")))))
+
+(deftest second-test
+    (testing "four"
+      (is (= 4 (captcha "1111")))))
+
+(deftest third-test
+    (testing "zero"
+      (is (= 0 (captcha "1234")))))
+
+(deftest ninth-test
+  (testing "nine"
+    (is (= 9 (captcha "91212129")))))
+
+(deftest aoc
+  (testing "aoc"
+    (is (= 1171 (captcha-amir intstream)))))
+
+(deftest aoc-2
+  (testing "aoc-2"
+    (is (= 1171 (captcha intstream)))))
+
+(deftest edge
+  (testing "edge"
+    (is (= 21 (captcha-amir "712377")))))
