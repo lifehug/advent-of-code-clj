b68171be149f5fd96324546bbad480a34582eefb
David Taylor <davidjamestaylor@gmail.com>
2017-12-12 22:25:24 -0800

    I hate this code, but it's done

--- src/aoc_2017/day3.clj
+++ src/aoc_2017/day3.clj
@@ -23,7 +23,13 @@
   (let [s (count m)
         c (neighbor-coords [x y] s)
         pv (add-neighbors c m)]
-    (assoc-in m [y x] pv)))
+    pv))
+
+(defn update-square [m [x y]] 
+  (let [ s (sum-neighbors m [x y])]
+    (if (= 0 s)
+        (assoc-in m [y x] 1)
+        (assoc-in m [y x] s))))
 
 (defn square-up [n] (if (even? n) (inc n) n))
 
@@ -34,10 +40,56 @@
     m))
 
 (defn down? [m [x y]]
-  (and (> (get-neighbor-value [[(inc x) y]] m) 0)
-       (= 0 (get-neighbor-value [[x (dec y)]] m))))
+  (if (or (>= (inc x) (count m)) (>= (inc y) (count m)))
+    false 
+    (and 
+      (> (first (get-neighbor-value [[(inc x) y]] m)) 0)
+      (= 0 (first (get-neighbor-value [[x (inc y)]] m))))))
+
+(defn up? [m [x y]]
+  (if (or (< (dec x) 0) (< (dec  y) 0))
+    false 
+    (and 
+      (> (first (get-neighbor-value [[(dec x) y]] m)) 0)
+      (= 0 (first (get-neighbor-value [[x (dec y)]] m))))))
+
+(defn left? [m [x y]]
+  (if (or (>= (inc y) (count m)) (< (dec x) 0))
+    false 
+    (and 
+      (> (first (get-neighbor-value [[x (inc y)]] m)) 0)
+      (= 0 (first (get-neighbor-value [[(dec x) y]] m))))))
+
+(defn right? [m [x y]]
+  (if (or (>= (inc x) (count m)) (< (dec y) 0))
+    false 
+    (or (= 0 (sum-neighbors m [x y])) 
+      (and 
+      (> (first (get-neighbor-value [[x (dec y)]] m)) 0)
+      (= 0 (first (get-neighbor-value [[(inc x) y]] m)))))))
 
 (defn next-position [m [x y]]
   (cond
-    (down? m [x y]) [x (dec y)]
+    (right? m [x y]) [(inc x) y]
+    (down? m [x y]) [x (inc y)]
+    (left? m [x y]) [(dec x) y]
+    (up? m [x y]) [x (dec y)]
     :else [-1 -1]))
+
+(defn finish-him 
+  ([n]
+   (let [s (-> n create-square) 
+         p (-> n Math/sqrt (/ 2) Math/floor int (->> (repeat 2) vec))
+         u (update-square s p)
+         np (next-position u p)] 
+       (finish-him u np n)))
+ ([m p n]
+  (let [sn (sum-neighbors m p) 
+        u (update-square m p) 
+        np (next-position u p)]
+  (if (>= n sn)
+      (recur u np n)
+      [sn u]))))
+
+
+
--- test/aoc_2017/day3_test.clj
+++ test/aoc_2017/day3_test.clj
@@ -6,13 +6,107 @@
     (testing "distance"
       (let [ expected [[0 0 0 0 0] [0 5 4 2 0] [0 0 1 1 0] [0 0 0 0 0][0 0 0 0 0]]
              given [[0 0 0 0 0] [0 0 4 2 0] [0 0 1 1 0] [0 0 0 0 0][0 0 0 0 0]]]
-            (is (= expected (sum-neighbors given [1 1]))))))
+            (is (= expected (update-square given [1 1]))))))
 
-(deftest next-step-test
+(deftest next-step-test-down
     (testing "go down"
       (let [ given [[0 0 0 0 0] 
                     [0 5 4 2 0] 
-                    [0 0 1 1 0] 
+                    [0 0 1 1 0]
                     [0 0 0 0 0] 
                     [0 0 0 0 0]]]
             (is (= [1 2] (next-position given [1 1]))))))
+
+(deftest next-step-test-down-2
+    (testing "go down"
+      (let [ given [[0 0 0 0 0] 
+                    [0 5 4 2 0] 
+                    [0 6 1 1 0]
+                    [0 0 0 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [1 3] (next-position given [1 2]))))))
+
+(deftest next-step-test-right
+    (testing "go right"
+      (let [ given [[0 0 0 0 0] 
+                    [0 5 4 2 0] 
+                    [0 6 1 1 0] 
+                    [0 7 0 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [2 3] (next-position given [1 3]))))))
+
+(deftest next-step-test-right-2
+    (testing "go right"
+      (let [ given [[0 0 0 0 0] 
+                    [0 5 4 2 0] 
+                    [0 6 1 1 0] 
+                    [0 7 8 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [3 3] (next-position given [2 3]))))))
+
+(deftest next-step-test-right-3
+    (testing "go right"
+      (let [ given [[0 0 0 0 0] 
+                    [0 0 0 0 0] 
+                    [0 0 1 0 0] 
+                    [0 0 0 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [3 2] (next-position given [2 2]))))))
+
+(deftest next-step-test-left-1
+    (testing "go left"
+      (let [ given [[0 0 0 0 0] 
+                    [0 0 0 2 0] 
+                    [0 0 1 1 0] 
+                    [0 0 0 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [2 1] (next-position given [3 1]))))))
+
+(deftest next-step-test-left-2
+    (testing "go left"
+      (let [ given [[0 0 0 0 0] 
+                    [0 0 4 2 0] 
+                    [0 0 1 1 0] 
+                    [0 0 0 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [1 1] (next-position given [2 1]))))))
+
+(deftest next-step-test-up
+    (testing "go up"
+      (let [ given [[0 0 0 0 0] 
+                    [0 0 0 0 0] 
+                    [0 0 1 1 0] 
+                    [0 0 0 0 0] 
+                    [0 0 0 0 0]]]
+            (is (= [3 1] (next-position given [3 2]))))))
+
+(deftest next-step-test-up-2
+    (testing "go up"
+      (let [ given [[0 0 0 0 0] 
+                    [0 5 4 2 0] 
+                    [0 10 1 1 0] 
+                    [0 11 23 25 26] 
+                    [0 0 0 0 0]]]
+            (is (= [4 2] (next-position given [4 3]))))))
+
+(deftest update-square-test
+    (testing "update square"
+      (let [ expected [[0 0 0 0 0] 
+                       [0 0 0 0 0] 
+                       [0 0 1 0 0] 
+                       [0 0 0 0 0] 
+                       [0 0 0 0 0]]]
+            (is (= expected (-> 25 create-square (update-square [2 2])))))))
+
+(deftest aoc-test-25
+    (testing "test 25"
+      (let [ given [[0 0 0 0 0] 
+                    [0 5 4 2 0] 
+                    [0 10 1 1 0] 
+                    [0 11 23 25 26] 
+                    [0 0 0 0 0]]]
+            (is (= [26 given] (finish-him 25))))))
+
+(deftest aoc-test
+    (testing "test 347991"
+            (is (= 349975 (first (finish-him 347991))))))
