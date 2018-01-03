2c4bc2b640ca45b216945dc74b5afe1ff842b4f0
David Taylor <davetaylor@apple.com>
2018-01-02 21:37:08 -0800

    added a generator

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.trace "0.7.9"]
-                 [org.blancas/kern "1.1.0"]])
+                 [org.blancas/kern "1.1.0"]
+                 [org.clojure/test.check "0.9.0"]])
--- test/aoc_2015/day1_test.clj
+++ test/aoc_2015/day1_test.clj
@@ -1,9 +1,24 @@
 (ns aoc-2015.day1-test
   (:require [clojure.test :refer :all]
-            [aoc-2015.day1 :refer :all]))
+            [aoc-2015.day1 :refer :all]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check :as tc]
+            [clojure.test.check.properties :as prop]
+            [clojure.tools.trace :refer [trace]]))
+
 (def intstream (-> "input/2015/day1.txt" slurp))
 
 (deftest test-floor-level
     (testing "three"
       (is (= 3 (floor "))(((((")))))
 
+(def test-with-generators 
+  (testing "generators"
+    (prop/for-all [input (gen/vector (gen/elements "()"))]
+    (let [floor-commands (apply str input)
+          command-count (count floor-commands)
+          floor-ups (count (filter #(= % \() floor-commands))
+          expected (- floor-ups (- command-count floor-ups))]
+      (is (=  expected (floor floor-commands)))))))
+
+(tc/quick-check 100 test-with-generators)
